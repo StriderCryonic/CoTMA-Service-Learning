aed8639111739245f1d1801141a720d778b1321d
Trisha Reddy <trisha.ys@btech.christuniversity.in>
2024-02-29 15:35:06 +0530

    Added AddC

--- cotma/src/components/AddC/AddC.jsx
+++ cotma/src/components/AddC/AddC.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import "./AddC.css";
 
+function handleSubmit(){
+  var cowId = document.getElementById("AddCow-cowId");
+  var age = document.getElementById("AddCow-age");
+  var sick = document.getElementById("AddCow-sick");
+  var preg = document.getElementById("AddCow-preg");
+  const newAddition = {"cowId": cowId,"age": age, "sick" : sick, "preg": preg};
+
+  let storedData = JSON.parse(localStorage.getItem('cowData'));
+  if (!storedData) { //if storeddata doesnt exist, create a new instance
+    storedData = [];
+  }
+  //add new data
+  storedData.push(newAddition);
+
+  //push updated array
+  localStorage.setItem('cowData', JSON.stringify(storedData));
+}
+
+
 const AddC = () => {
   
   return (
@@ -8,11 +27,11 @@ const AddC = () => {
     <div class="login-page">
     <div class="form">
       
-      <form class="login-form">
-        <input type="text" placeholder="cow code"/>
-        <input type="text" placeholder="age"/>   
-        <input type="text" placeholder='health issues'/>
-        <input type="text" id="preg" placeholder='maternity status'/>
+      <form class="login-form" onSubmit={handleSubmit}>
+        <input type="text" id = 'AddCow-cowId' placeholder="cow code"/>
+        <input type="text" id = 'AddCow-age' placeholder="age"/>   
+        <input type="text" id = 'AddCow-sick' placeholder='health issues'/>
+        <input type="text" id = 'AddCow-preg' placeholder='maternity status'/>
         
            
         <button>register</button>
