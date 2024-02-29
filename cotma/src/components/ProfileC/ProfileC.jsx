2a428c9498f2fa2e29822e1228632339a13bfbc0
Yaseen-christy <hokagenaruto2679@gmail.com>
2024-02-29 22:18:38 +0530

    YOL

--- cotma/src/components/ProfileC/ProfileC.jsx
+++ cotma/src/components/ProfileC/ProfileC.jsx
@@ -4,22 +4,24 @@ import "./ProfileC.css"
 function findDetails(){
   let CowCode = localStorage.getItem('temp');
   console.log(CowCode);
-  let storedData = JSON.parse(localStorage.getItem('workerData'));
+  let storedData = JSON.parse(localStorage.getItem('cowData'));
   if (!storedData) { //if storeddata doesnt exist, error
       console.log("ERROR!!! NO DATA AVAILABLE TO SEARCH.");
   }
   else{
     let matchingUser = storedData.find(cow => cow.cowId  === CowCode);
     console.log(matchingUser);
+    return matchingUser;
   }
 }
 
 const ProfileC=() =>{
-
+  
 return(
     <div class="login-page">
     <div class="form">
-    <div id="test">Testings</div>
+    
+    <div id="test">Id:</div>
     {findDetails()}
     </div>
   </div>
